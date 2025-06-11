5775840a2000252c1b5cc7f88e57679097e0d0e3
sander-1105 <sander@57blocks.com>
2025-06-11 17:39:59 +0800

    Remove commented test lines from hello.py file

--- hello.py
+++ hello.py
@@ -4,12 +4,6 @@ def hello():
 def helloworld():
     print('hello world!')
 
-# test line1
-# test line2
-# test line3
-# test line4
-# test line5
-
 if __name__ == '__main__':
     hello()
     helloworld()
