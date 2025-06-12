5173995a69d642b9b48b3f309b9b59ac5855d2fd
sander-1105 <sander@57blocks.com>
2025-06-12 13:51:29 +0800

    Update hello.py
    Refactor hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,8 @@
 def hello():
     print('hello world!')
-    print('hello world!')
 
 def helloworld():
     print('hello world!')
-    print('hello world aaaa!')
-    print('hello world aaaa!')
-    print('hello world aaaa!')
-    print('hello world aaaa!')
-    print('hello world aaaa!')
 
 if __name__ == '__main__':
     hello()
