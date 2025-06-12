2c9f024e991db97275e33326d1dba6ebf94ce88a
sander-1105 <sander@57blocks.com>
2025-06-12 11:05:34 +0800

    Update hello.py
    u

--- hello.py
+++ hello.py
@@ -7,7 +7,6 @@ def helloworld():
     print('hello world aaaa!')
     print('hello world aaaa!')
     print('hello world aaaa!')
-    print('hello world aaaa!')
 
 if __name__ == '__main__':
     hello()
